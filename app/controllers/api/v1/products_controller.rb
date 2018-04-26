8ffac7576be0dc20ff1ed10e8fc3ca7ab75b5862
sbassmed <sebastiancamilomarin@gmail.com>
2018-04-25 20:26:14 -0500

    Se agrego el metodo create , con los mensajes HTML y formato JSON, tambien se agrego skip_before_action :verify_authenticity_token ,se probo el endopoint con POSTMAN

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,19 @@
 class Api::V1::ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
     products = Product.all
     render json: products
+  end
+  def create
+    product = Product.new(products_params)
+    if product.save
+      render json: product , status: 201
+    else
+      render json: {errors: product.errors } , status: 422
+    end
+  end
+  private
+  def products_params
+    params.require(:product).permit(:name , :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       resources :products
     end
   end
-
+resources :products
 
 
   # The priority is based upon order of creation: first created -> highest priority.
